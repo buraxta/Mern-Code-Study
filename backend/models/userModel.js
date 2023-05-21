72c34a4382b683b2367d919a3c39285b3a71ec75
Burak Güllüler <burakgulluler@gmail.com>
2023-05-21 16:07:39 +0300

    Auth user endpoint

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,6 +28,10 @@ const userSchema = mongoose.Schema({
         this.password = await bcrypt.hash(this.password, salt);
     });
 
+    userSchema.methods.matchPassword = async function (enteredPassword) {
+        return await bcrypt.compare(enteredPassword, this.password);
+    };
+
     const User = mongoose.model('User', userSchema);
 
     export default User;
