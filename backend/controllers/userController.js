fc30af03128e54c54c70eccbd8cad188ce5c1397
Burak Güllüler <burakgulluler@gmail.com>
2023-05-21 15:50:25 +0300

    generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 // @desc    Auth user/set token
 // route    POST /api/users/auth
@@ -24,6 +25,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({ name, email, password });
 
   if (user) {
+    generateToken(res, user._id);
     res.status(201).json({ 
       _id: user._id,
       name: user.name,
@@ -33,7 +35,7 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Invalid user data");
   };
-  
+
 });
 
 // @desc    Logout user
