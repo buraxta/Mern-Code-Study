999e5e5bc2576abaf185f6802d9150522e7bc386
Burak Güllüler <burakgulluler@gmail.com>
2023-05-21 11:58:27 +0300

    Register User Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,8 +12,28 @@ const authUser = asyncHandler(async (req, res) => {
 // route    POST /api/users/auth
 // access   Public
 const registerUser = asyncHandler(async (req, res) => {
-  console.log(req.body);
-  res.status(200).json({ message: "Register User" });
+  const { name, email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    res.status(201).json({ 
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+     });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  };
+  
 });
 
 // @desc    Logout user
