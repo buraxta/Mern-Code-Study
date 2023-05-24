82a407f79a6fb5c000106e658967d471dde55f06
Burak Güllüler <burakgulluler@gmail.com>
2023-05-25 00:05:26 +0300

    react router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header";
-
+import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
 const App = () => {
   return (
     <>
       <Header />
-      <h1>MERN Auth</h1>
+      <Container className="my-2">
+      < Outlet />
+      </Container>
     </>
   );
 };
